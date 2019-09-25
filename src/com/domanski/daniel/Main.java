fffe409acb9dc02983cf617f96f7b64e81b62bbc
Michal Maksymiuk <nf959@o2.pl>
2019-09-25 17:15:34 +0200

    Dodano metodę czytającą dane wejściowe

--- src/com/domanski/daniel/Main.java
+++ src/com/domanski/daniel/Main.java
@@ -1,8 +1,17 @@
 package com.domanski.daniel;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Kalkulator");
+    }
+
+    public static double pobierzLiczbe(){
+
+        Scanner myObj = new Scanner(System.in);
+        double liczba = Double.parseDouble(myObj.nextLine());
+        return liczba;
     }
 }
