8bd7367ecad7dae0799ec7b0732fd87003d0572e
Michal Maksymiuk <nf959@o2.pl>
2019-09-25 17:33:54 +0200

    Dodano pobieranie znaku operacji

--- src/com/domanski/daniel/Main.java
+++ src/com/domanski/daniel/Main.java
@@ -6,10 +6,17 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Kalkulator");
+
     }
 
     public static double pobierzLiczbe(){
         Scanner myObj = new Scanner(System.in);
         return Double.parseDouble(myObj.nextLine());
+    }
+
+    public static String pobierzOperacje(){
+        System.out.println("Wybierz operację jaką chcesz wykonać +, -, *, /");
+        Scanner obj = new Scanner(System.in);
+        return obj.nextLine();
     }
 }
