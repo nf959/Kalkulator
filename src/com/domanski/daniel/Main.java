705b1dc5976735e0d334dee6d1034042c4362a06
Michal Maksymiuk <nf959@o2.pl>
2019-09-25 17:38:28 +0200

    Dodano switch + prototyp dodawania i odejmowania

--- src/com/domanski/daniel/Main.java
+++ src/com/domanski/daniel/Main.java
@@ -6,15 +6,28 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Kalkulator");
+        String operacja;
+        do {
+            operacja = pobierzOperacje();
+            switch (operacja) {
+                case "+":
+                    System.out.println("Dodawanie");
+                    break;
+                case "-":
+                    System.out.println("Odejmowanie");
+                    break;
+            }
+        } while (!operacja.equals("x"));
+
 
     }
 
-    public static double pobierzLiczbe(){
+    public static double pobierzLiczbe() {
         Scanner myObj = new Scanner(System.in);
         return Double.parseDouble(myObj.nextLine());
     }
 
-    public static String pobierzOperacje(){
+    public static String pobierzOperacje() {
         System.out.println("Wybierz operację jaką chcesz wykonać +, -, *, /");
         Scanner obj = new Scanner(System.in);
         return obj.nextLine();
