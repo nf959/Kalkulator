2900bac4fe8a70478e2117eecd29450fa05fb508
Marta Kobryn <marta.kobryn@o2.pl>
2019-09-25 17:56:02 +0200

    Dodano metodę odejmij

--- src/com/domanski/daniel/Kalkulator.java
+++ src/com/domanski/daniel/Kalkulator.java
@@ -4,4 +4,8 @@ public class Kalkulator {
     public static double dodaj(double a, double b){
         return a + b;
     }
+
+    public static double odejmij(double a, double b){
+        return a - b;
+    }
 }
