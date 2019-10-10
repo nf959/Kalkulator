152132666e759fce6b072fad423bdbf29f0fb6c7
nf959 <42828168+nf959@users.noreply.github.com>
2019-10-10 11:57:33 +0200

    Zmieniono mordke na smutna

--- src/com/domanski/daniel/Main.java
+++ src/com/domanski/daniel/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Kalkulator :)");
+        System.out.println("Kalkulator :(");
         String operacja;
         do {
             System.out.print("Podaj liczbe1 = ");
